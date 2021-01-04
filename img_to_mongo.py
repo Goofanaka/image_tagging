a8c55aa22f6282bd69f22f64810dcb7d4b848f86
KIHOON71 <caleb9510@gmail.com>
2021-01-04 23:44:06 +0900

    '[FIX]connect_db_in_main'

---
+++ img_to_mongo.py
@@ -0,0 +1,20 @@
+from mongoengine import *
+
+connect('image_tag_data', 'default', host="mongodb+srv://GFNK:GFNK@gufanaka.orv7h.mongodb.net/image_tag_data?retryWrites=true&w=majority")
+
+class Image(Document):
+    name = StringField(unique=True, required=True)
+    image = ImageField(required=True)
+ 
+    meta = {
+        'id' : name
+    }
+
+    # example = Image(name = 'example')
+    # with open('C:\coupang_crawler\image\\2020_12_30_1.jpg', 'rb') as img:
+    #     example.image.replace(img, filename = 'example.jpg')
+    #     example.save()
+
+    
+
+        
